4380491960b9b47e58dd05fbd0ce1245ff5a0ced
Michael Kennedy <mikeckennedy@gmail.com>
2018-12-11 16:37:26 -0800

    Bill tracker, now with viewmodels.

--- days/081-084-unit-testing/demo/billtracker/billtracker/views/default.py
+++ days/081-084-unit-testing/demo/billtracker/billtracker/views/default.py
@@ -4,60 +4,42 @@ from pyramid.response import Response
 from pyramid.view import view_config
 
 from billtracker.data import repository
+from billtracker.viewmodels.default.index_viewmodel import IndexViewModel
+from billtracker.viewmodels.default.bill_details_viewmodel import BillDetailsViewModel
 
 
 @view_config(route_name='home', renderer='../templates/home/default.pt')
-def home(_: Request):
-    user_id = 1  # probably get from a cookie?
-
-    user = repository.get_user_by_id(user_id)
-    return {
-        'user': user,
-    }
+def home(request: Request):
+    vm = IndexViewModel(request, user_id=1)
+    return vm.to_dict()
 
 
 @view_config(route_name='details',
              renderer='../templates/home/details.pt',
              request_method='GET')
 def details_get(request: Request):
-    bill_id = int(request.matchdict.get('bill_id'))
-    bill = repository.get_bill_by_id(bill_id)
-    if not bill:
+    vm = BillDetailsViewModel(request, user_id=1)
+
+    if not vm.bill:
         return Response(status=404)
 
-    user_id = 1  # probably get from a cookie?
-    user = repository.get_user_by_id(user_id)
-
-    return {
-        'user': user,
-        'bill': bill,
-        'error': None
-    }
+    return vm.to_dict()
 
 
 @view_config(route_name='details',
              renderer='../templates/home/details.pt',
              request_method='POST')
 def details_post(request: Request):
-    bill_id = int(request.matchdict.get('bill_id'))
-    bill = repository.get_bill_by_id(bill_id)
-    if not bill:
+    vm = BillDetailsViewModel(request, user_id=1)
+
+    if not vm.bill:
         return Response(status=404)
 
-    user_id = 1  # probably get from a cookie?
-    user = repository.get_user_by_id(user_id)
+    vm.from_form()
 
-    amount = int(request.POST.get('amount', -1))
-    if amount < 0 or amount > bill.total-bill.paid:
-        error = 'Your amount must be more the zero and less than what you owe.'
+    if vm.error:
+        return vm.to_dict()
 
-        return {
-            'user': user,
-            'bill': bill,
-            'error': error,
-            'amount': amount
-        }
+    repository.add_payment(vm.amount, vm.bill_id)
 
-    repository.add_payment(amount, bill_id)
-
-    return HTTPFound(location='/bill/{}'.format(bill_id))
+    return HTTPFound(location='/bill/{}'.format(vm.bill_id))
