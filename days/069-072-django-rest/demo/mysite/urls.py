6e4256d67216276d02571e28a79bda9a0191bccd
Bob Belderbos <bobbelderbos@gmail.com>
2019-01-25 11:58:09 +0100

    add API schema

--- days/069-072-django-rest/demo/mysite/urls.py
+++ days/069-072-django-rest/demo/mysite/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
 
+TITLE = 'Quotes API'
+
+schema_view = get_schema_view(title=TITLE)
 
 urlpatterns = [
     path('', include('quotes.urls')),
@@ -9,4 +13,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('schema/', schema_view),
 ]
