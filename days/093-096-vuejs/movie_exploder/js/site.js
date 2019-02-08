4bdb951e4979eb9c99b70d56fe6868dd085c1546
Michael Kennedy <mikeckennedy@gmail.com>
2019-02-07 16:31:59 -0800

    now with top 10 link.

--- days/093-096-vuejs/movie_exploder/js/site.js
+++ days/093-096-vuejs/movie_exploder/js/site.js
@@ -3,7 +3,16 @@
 app = new Vue({
     el: '#app',
     data: {
-        search_text: "initial value",
+        search_text: null,
         movies: movies_temp.hits
+    },
+    methods: {
+        search: function() {
+            let text = this.search_text
+            console.log("Would have searched for " + text)
+        },
+        top_10: function() {
+            console.log("Would have loaded top 10")
+        }
     }
 })
