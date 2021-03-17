511d08014db2c79b83c0f9613d2b919588783c9f
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2021-03-17 09:28:40 -0400

    Set the `time` since it's used in template

--- days/001-004-flask-intro/code/program/routes.py
+++ days/001-004-flask-intro/code/program/routes.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from flask import render_template
 from program import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Template Demo')
+    return render_template('index.html', title='Template Demo', time=datetime.now())
 
 @app.route('/100Days')
 def p100days():
