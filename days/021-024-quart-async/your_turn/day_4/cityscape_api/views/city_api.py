d79a63fde54ead99bec5ff826a7fd0bf1b4e77be
Michael Kennedy <mikeckennedy@gmail.com>
2021-11-29 16:37:28 -0800

    Fixes #59 - Day 24 does not work with Flask 2.x.

--- days/021-024-quart-async/your_turn/day_4/cityscape_api/views/city_api.py
+++ days/021-024-quart-async/your_turn/day_4/cityscape_api/views/city_api.py
@@ -1,7 +1,7 @@
 import flask
 from services import weather_service, sun_service, location_service
 
-blueprint = flask.blueprints.Blueprint(__name__, __name__)
+blueprint = flask.blueprints.Blueprint(__name__.replace('.', '_'), __name__)
 
 
 @blueprint.route('/api/events/<city>/<state>/<country>', methods=['GET'])
--- days/021-024-quart-async/your_turn/day_4/cityscape_api/views/home.py
+++ days/021-024-quart-async/your_turn/day_4/cityscape_api/views/home.py
@@ -1,6 +1,6 @@
 import flask
 
-blueprint = flask.blueprints.Blueprint(__name__, __name__)
+blueprint = flask.blueprints.Blueprint(__name__.replace('.', '_'), __name__)
 
 
 @blueprint.route('/')
